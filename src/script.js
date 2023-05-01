9222bdcf36878ddc3d15fb5f0b3f7e5a83b3aa7b
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2023-04-30 23:51:35 -0300

    lidando com pixel ratio

--- src/script.js
+++ src/script.js
@@ -35,6 +35,7 @@ window.addEventListener('resize', () =>{
 	camera.updateProjectionMatrix()
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
